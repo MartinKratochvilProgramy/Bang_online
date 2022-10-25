228a59e3b32095f95aae89a1a3e46eebe4e3e21b
martin.kratochvil <martvil96@gmail.com>
2022-10-25 12:59:57 +0200

    working message rooms

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
+var uuid = require('uuid');
 const cors = require("cors");
 const io = new Server(server, {
   cors: {
@@ -14,8 +15,17 @@ const PORT = 3001;
 app.use(cors());
 
 let rooms = {
-  jedna: [],
-  dva: []
+  room: {
+    users: [{
+        username: "sbeve",
+        id: "id"
+    }],
+    messages: [{
+        username: "sbeve",
+        message: "message content",
+        id: uuid.v4()
+    }]
+}
 }
 
 io.on("connection", (socket) => {
@@ -28,19 +38,19 @@ io.on("connection", (socket) => {
       username: data.username,
       id: socket.id
     };
+    rooms[roomName].users.push(newUser);
+    console.log(rooms[roomName]);
 
-    rooms[roomName].push(newUser);
-
-    io.to(data.currentRoom).emit("show_users", rooms[roomName]);
+    io.to(data.currentRoom).emit("get_room", rooms[roomName]);
   });
 
   socket.on("disconnect", () => {
     // user disconnected by closing the browser
     for (var room in rooms) {
       for (let i = 0; i < rooms[room].length; i++) {
-        if(rooms[room][i].id === socket.id) {
-          rooms[room].splice(i, 1);
-          io.to(room).emit("show_users", rooms[room]);
+        if(rooms[room].users[i].id === socket.id) {
+          rooms[room].users.splice(i, 1);
+          io.to(room).emit("get_room", rooms[room]);
           return;
         }
       }
@@ -49,13 +59,25 @@ io.on("connection", (socket) => {
 
   socket.on("leave_room", data => {
     const roomName = data.currentRoom;
-    rooms[roomName].splice(rooms[roomName].indexOf(data.username), 1);
-    io.to(roomName).emit("show_users", rooms[roomName]);
+    rooms[roomName].users.splice(rooms[roomName].users.indexOf(data.username), 1);
+    io.to(roomName).emit("get_room", rooms[roomName]);
   });
 
   socket.on("create_room", roomName => {
-    rooms[roomName] = [];
-    socket.emit("rooms", rooms);
+    rooms[roomName] = {
+      users: [],
+      messages: []
+    };
+    io.emit("rooms", rooms);
+  })
+
+  socket.on("send_message", data => {
+    rooms[data.currentRoom].messages.push({
+      username: data.username,
+      message: data.message,
+      id: uuid.v4()
+    })
+    io.to(data.currentRoom).emit("get_room", rooms[data.currentRoom]);
   })
 });
 
